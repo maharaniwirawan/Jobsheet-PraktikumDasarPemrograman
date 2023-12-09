cf4b92405b7e2c8404b720cfb1fbde8a9b195ffe
maharaniwirawan <maharaniwr246@gmail.com>
2023-12-09 15:04:20 +0700

    commit

--- Quiz.java
+++ Quiz.java
@@ -1,29 +1,3 @@
-import java.util.Scanner;
-import java.util.Random;
-
-public class Quiz {
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        Random rand = new Random();
-        char menu = 'y';
-        do {
-            int number = rand.nextInt(10) + 1;
-            boolean success = false;
-            do {
-                System.out.print("Tebak angka (1-10): ");
-                int answer = input.nextInt();
-                input.nextLine();
-                success = (answer == number);
-            } while (!success);
-            System.out.print("Apakah Anda ingin mengulang permainan (Y/T): ");
-            menu = input.next().charAt(0);
-            input.nextLine();
-        } while (menu == 'Y' || menu == 'y');
-    }
-}
-
-// #MODIFIKASI
 // import java.util.Scanner;
 // import java.util.Random;
 
@@ -33,32 +7,58 @@ public class Quiz {
 //         Scanner input = new Scanner(System.in);
 //         Random rand = new Random();
 //         char menu = 'y';
-
 //         do {
 //             int number = rand.nextInt(10) + 1;
-//             int answer; 
 //             boolean success = false;
-
 //             do {
 //                 System.out.print("Tebak angka (1-10): ");
-//                 answer = input.nextInt();
+//                 int answer = input.nextInt();
 //                 input.nextLine();
-
-//                 if (answer < number) {
-//                     System.out.println("Tebakan terlalu kecil. Coba lagi!");
-//                 } else if (answer > number) {
-//                     System.out.println("Tebakan terlalu besar. Coba lagi!");
-//                 } else {
-//                     System.out.println("Selamat! Tebakan benar!");
-//                 }
-
-//             } while (answer != number);
-
+//                 success = (answer == number);
+//             } while (!success);
 //             System.out.print("Apakah Anda ingin mengulang permainan (Y/T): ");
 //             menu = input.next().charAt(0);
 //             input.nextLine();
-
 //         } while (menu == 'Y' || menu == 'y');
 //     }
 // }
+
+// #MODIFIKASI
+import java.util.Scanner;
+import java.util.Random;
+
+public class Quiz {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        char menu = 'y';
+
+        do {
+            int number = rand.nextInt(10) + 1;
+            int answer; 
+            boolean success = false;
+
+            do {
+                System.out.print("Tebak angka (1-10): ");
+                answer = input.nextInt();
+                input.nextLine();
+
+                if (answer < number) {
+                    System.out.println("Tebakan terlalu kecil. Coba lagi!");
+                } else if (answer > number) {
+                    System.out.println("Tebakan terlalu besar. Coba lagi!");
+                } else {
+                    System.out.println("Selamat! Tebakan benar!");
+                }
+
+            } while (answer != number);
+
+            System.out.print("Apakah Anda ingin mengulang permainan (Y/T): ");
+            menu = input.next().charAt(0);
+            input.nextLine();
+
+        } while (menu == 'Y' || menu == 'y');
+    }
+}
 
